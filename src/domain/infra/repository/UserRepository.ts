1c6ea5ed79e28544cee69a3b75df7874ac48facc
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2022-11-23 05:20:40 -0300

    style: yagni - removing update user cause im not using it right now

--- src/domain/infra/repository/UserRepository.ts
+++ src/domain/infra/repository/UserRepository.ts
@@ -3,6 +3,5 @@ import User from "../../entity/User";
 export default interface UserRepositoryInterface {
   save(user: User): Promise<void>;
   getUserByEmail(email: string): Promise<User>;
-  update(user: User): Promise<void>;
   isEmailTaken(email: string): Promise<boolean>;
 }
--- src/infra/repository/memory/MemoryUserRepository.ts
+++ src/infra/repository/memory/MemoryUserRepository.ts
@@ -12,11 +12,6 @@ export default class MemoryUserRepository implements UserRepositoryInterface {
     return this.users.find((user) => user.email === email);
   }
 
-  async update(user: User): Promise<void> {
-    const index = this.users.indexOf(user);
-    this.users[index] = user;
-  }
-
   async isEmailTaken(email: string): Promise<boolean> {
     const result = this.users.find((user) => user.email === email);
     return result !== undefined;
