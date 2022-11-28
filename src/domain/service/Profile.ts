240b8dba527122c78ec44c37b30e03605e20f179
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2022-11-28 10:57:04 -0300

    style: cleaning profile service code

--- src/domain/service/Profile.ts
+++ src/domain/service/Profile.ts
@@ -4,19 +4,21 @@ import Profile from "../entity/Profile";
 export default class ProfileService {
   static follow(follower: Profile, followee: Profile) {
     if (follower.profileId === followee.profileId) throw new BadRequest("You can't follow yourself");
-    if (follower.getFollowing().includes(followee.profileId)) {
-      throw new BadRequest("You are already following this profile");
-    }
+    const isFollowing = this.isFollowing(follower, followee);
+    if (isFollowing) throw new BadRequest("You are already following this profile");
     follower.addFollowing(followee.profileId);
     followee.addFollower(follower.profileId);
   }
 
   static unfollow(unfollower: Profile, unfollowee: Profile) {
     if (unfollower.profileId === unfollowee.profileId) throw new BadRequest("You can't unfollow yourself");
-    if (!unfollower.getFollowing().includes(unfollowee.profileId)) {
-      throw new BadRequest("You are not following this profile");
-    }
+    const isFollowing = this.isFollowing(unfollower, unfollowee);
+    if (!isFollowing) throw new BadRequest("You are not following this profile");
     unfollower.removeFollowing(unfollowee.profileId);
     unfollowee.removeFollower(unfollower.profileId);
+  }
+
+  private static isFollowing(follower: Profile, followee: Profile) {
+    return follower.getFollowing().includes(followee.profileId);
   }
 }
