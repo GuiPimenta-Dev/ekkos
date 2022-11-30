7c0c9011f69b8d362c547cb6773ccf83fa5babc1
wcarugatti <wilson.tibia.valentin@gmail.com>
2022-11-30 13:30:02 -0300

    refactor: event factory types

--- src/domain/event/EventFactory.ts
+++ src/domain/event/EventFactory.ts
@@ -1,30 +1,40 @@
-import { InviteAcceptedPayload, InviteDeclinedPayload, UserCreatedPayload } from "./Types";
+import Band from "../entity/Band";
 
 export interface Event<T = unknown> {
   name: string;
   payload: T;
 }
 
-export type InviteAccepted = Event<InviteAcceptedPayload>;
-export type InviteDeclined = Event<InviteDeclinedPayload>;
-export type UserCreated = Event<UserCreatedPayload>;
+export type InviteAccepted = Event<{
+  profileId: string;
+  band: Band;
+  role: string;
+}>;
+export type InviteDeclined = Event<{
+  profileId: string;
+  band: Band;
+  role: string;
+}>;
+export type UserCreated = Event<{
+  email: string;
+}>;
 
 export default class EventFactory {
-  static emitInviteAccepted(payload: InviteAcceptedPayload): InviteAccepted {
+  static emitInviteAccepted(payload: InviteAccepted["payload"]): InviteAccepted {
     return {
       name: "InviteAccepted",
       payload,
     };
   }
 
-  static emitInviteDeclined(payload: InviteDeclinedPayload): InviteDeclined {
+  static emitInviteDeclined(payload: InviteDeclined["payload"]): InviteDeclined {
     return {
       name: "InviteDeclined",
       payload,
     };
   }
 
-  static emitUserCreated(payload: UserCreatedPayload): UserCreated {
+  static emitUserCreated(payload: UserCreated["payload"]): UserCreated {
     return {
       name: "UserCreated",
       payload,
